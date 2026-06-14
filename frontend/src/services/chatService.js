3b577bf6c6de55077817fa34b9e6da35cc502769
harshit9651 <programming966051@gmail.com>
2026-06-14 22:56:38 +0530

    adding vector DB

--- frontend/src/services/chatService.js
+++ frontend/src/services/chatService.js
@@ -1,31 +1,21 @@
+import axios from "axios";
+const API_URL =  import.meta.env.VITE_API_URL;
 export const askQuestion = async (question) => {
-
-  await new Promise((resolve) =>
-    setTimeout(resolve, 2000)
+  const { data } = await axios.post(
+     `${API_URL}/api/chat`,
+    {
+      question,
+    }
   );
 
   return {
     id: crypto.randomUUID(),
-
     role: "assistant",
-
-    content: `
-# PMCF
-
-PMCF stands for Post Market Clinical Follow-Up.
-
-It is required under EU MDR.
-
-## Key Points
-
-- Continuous monitoring
-- Clinical safety
-- Regulatory compliance
-`,
-
-    sources: [
-      "EU MDR Article 83",
-      "PMCF Guidance Document"
-    ]
+    content: data.answer,
+    timestamp: Date.now(),
+    sources:
+      data.sources?.map(
+        (item) => item.source
+      ) || [],
   };
 };
